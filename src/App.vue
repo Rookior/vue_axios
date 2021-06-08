ce0fb65b09bf6f1d71f514083635559764ba1a5b
Rookior <1757629766@qq.com>
2021-06-08 14:44:44 +0800

    增加联系人列表菜单

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+		<router-link to="/">Home</router-link> | 
+		<router-link to="/about">About</router-link> | 
+		<router-link to="/contactList">联系人列表</router-link>
     </div>
     <router-view/>
   </div>
